d1ae5820060d6e8a17576c4d2f8e34c9a258201c
_c0da_ <duvictor514@gmail.com>
2018-01-27 16:56:25 -0800

    Fixed timed autosave bug.

--- app/src/main/java/trc3543/trcscoutingapp/About.java
+++ app/src/main/java/trc3543/trcscoutingapp/About.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class About extends AppCompatActivity {
+public class About extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setTitle("About This App");
