106bcfab32ae5f1bf462272cfb7145548a5dc0f5
_c0da_ <duvictor514@gmail.com>
2018-12-06 19:36:17 -0800

    Updated all copyrights.

--- app/src/main/java/trc3543/trcscoutingapp/DataStore.java
+++ app/src/main/java/trc3543/trcscoutingapp/DataStore.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2018 Victor Du, Titan Robotics Club
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package trc3543.trcscoutingapp;
 
 import android.os.Environment;
@@ -21,29 +43,6 @@ import java.util.Date;
 @SuppressWarnings("All")
 public class DataStore extends AppCompatActivity
 {
-    /**
-     *
-     *  Copyright (c) 2018 Titan Robotics Club, _c0da_ (Victor Du)
-     *
-     *	Permission is hereby granted, free of charge, to any person obtaining a copy
-     *	of this software and associated documentation files (the "Software"), to deal
-     *	in the Software without restriction, including without limitation the rights
-     *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-     *	copies of the Software, and to permit persons to whom the Software is
-     *	furnished to do so, subject to the following conditions:
-     *
-     *	The above copyright notice and this permission notice shall be included in all
-     *	copies or substantial portions of the Software.
-     *
-     *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-     *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-     *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-     *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-     *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-     *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-     *	SOFTWARE.
-     */
-
     static final String DATA_FOLDER_NAME = "TrcScoutingApp";
 
     static final String CSV_HEADER = "Contains Your Team, Date, Match #, Competition Type, Team Number, Spectating Team, Starting Position, AT-Robot Lowered, AT-Mineral Displaced, AT-Mineral Correct, AT-Marker Deployed, AT-Parked In Crater, TO-Depot Score, TO-Lander Score, EG-Ending Location, Match Won, Autonomous Notes, TeleOp Notes";
