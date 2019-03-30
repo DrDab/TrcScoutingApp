07e3960e2aec622d4213fa9316ca51b10614b660
DrDab <duvictor514@gmail.com>
2019-03-29 23:50:35 -0700

    Added the save button

--- app/src/main/java/trc3543/trcscoutingapp/SetMatchInfo.java
+++ app/src/main/java/trc3543/trcscoutingapp/SetMatchInfo.java
@@ -35,6 +35,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -139,6 +141,7 @@ public class SetMatchInfo extends AppCompatActivity
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         final PagerAdapter adapter = new PagerAdapter
                 (getSupportFragmentManager(), tabLayout.getTabCount());
+
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -157,7 +160,12 @@ public class SetMatchInfo extends AppCompatActivity
 
             }
         });
+
         // done initializing tabbed layout.
+
+        // initialize all ui elements.
+
+        // done initializing ui elements.
 
         try
         {
@@ -272,6 +280,32 @@ public class SetMatchInfo extends AppCompatActivity
             }
         }
         return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_set_match_info2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_save)
+        {
+            confirmTypes(null);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /*
