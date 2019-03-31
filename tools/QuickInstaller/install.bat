a5a6bc740ee6b232983b61bbd2a29ccb674aad58
DrDab <duvictor514@gmail.com>
2019-03-30 23:14:33 -0700

    Make the installer actually work

---
+++ tools/QuickInstaller/install.bat
@@ -0,0 +1,23 @@
+@echo off
+:start
+echo TRC Scouting App Installer
+echo (C) Titan Robotics Club 2019
+:prompt
+echo.
+echo       Choice Menu
+echo [1] - Install app on phone
+echo [9] - Exit 
+SET /P _inputname=Choice: 
+IF "%_inputname%"=="1" GOTO :install
+IF "%_inputname%"=="9" GOTO :done
+:confused
+echo Please enter a valid choice.
+goto :prompt
+:done
+echo Bye!
+exit
+:install
+echo Installing App...
+adb install -r app.apk
+echo Done
+goto :start
