a5c3174f20c9d84334b6af42bd7e99f50ce9d6db
DrDab <duvictor514@gmail.com>
2019-04-02 15:55:59 -0700

    Added pit data functionality to quickpuller

--- tools/LinuxTools/QuickPuller-Linux/grabber.sh
+++ tools/LinuxTools/QuickPuller-Linux/grabber.sh
@@ -3,20 +3,28 @@ echo "(C) Titan Robotics Club 2019"
 while true; do 
 	echo
 	echo "      Choice Menu"
-	echo "[1] - Pull data from phone"
+	echo "[1] - Pull match data from phone"
+	echo "[2] - Pull pit data from phone"
 	echo "[9] - Exit "
 	read input
 	if [ "$input" = "1" ];
 	then
-		echo "Pulling data from phone..."
-		mkdir outputs
+		echo "Pulling match data from phone..."
+		mkdir matchdata
  		adb shell ls sdcard/TrcScoutingApp/*.csv | tr '\r' ' ' | xargs -n1 adb pull
-		mv *.csv outputs
+		mv *.csv matchdata
 		echo "Done"
 	elif [ "$input" = "9" ];
 	then
 		echo "Bye!"
 		exit
+	elif [ "$input" = "2" ];
+	then
+		echo "Pulling pit data from phone..."
+		mkdir pitdata
+ 		adb shell ls sdcard/TrcPitScouting/*.csv | tr '\r' ' ' | xargs -n1 adb pull
+		mv *.csv pitdata
+		echo "Done"
 	else
 		echo "Please enter a valid choice."
 	fi
--- tools/WindowsTools/QuickPuller-Windows/grabber.bat
+++ tools/WindowsTools/QuickPuller-Windows/grabber.bat
@@ -6,11 +6,13 @@ taskkill /im adb.exe /f
 :prompt
 echo.
 echo       Choice Menu
-echo [1] - Pull data from phone
+echo [1] - Pull match data from phone
+echo [2] - Pull pit data from phone
 echo [9] - Exit 
 SET /P _inputname=Choice: 
 IF "%_inputname%"=="1" GOTO :pull
 IF "%_inputname%"=="9" GOTO :done
+IF "%_inputname%"=="2" GOTO :pullpit
 :confused
 echo Please enter a valid choice.
 goto :prompt
@@ -18,9 +20,16 @@ goto :prompt
 echo Bye!
 exit
 :pull
-echo Pulling data from phone...
-mkdir outputs
+echo Pulling match data from phone...
+mkdir matchdata
 adb shell ls sdcard/TrcScoutingApp/*.csv | tr '\r' ' ' | xargs -n1 ./adb pull
-for /r "." %%x in (*.csv) do move "%%x" "outputs"
+for /r "." %%x in (*.csv) do move "%%x" "matchdata"
+echo Done
+goto :start
+:pullpit
+echo Pulling pit data from phone...
+mkdir pitdata
+adb shell ls sdcard/TrcPitScouting/*.csv | tr '\r' ' ' | xargs -n1 ./adb pull
+for /r "." %%x in (*.csv) do move "%%x" "pitdata"
 echo Done
 goto :start
