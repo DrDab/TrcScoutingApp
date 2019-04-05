5ad390ad7e0373d2bba02739261f7f74ee6cd176
DrDab <duvictor514@gmail.com>
2019-04-05 16:29:37 -0700

    Make sure function to get most recent file works

--- tools/WindowsTools/QuickPuller-Windows/grabber.bat
+++ tools/WindowsTools/QuickPuller-Windows/grabber.bat
@@ -8,15 +8,17 @@ echo.
 echo       Choice Menu
 echo [1] - Pull all match data from phone
 echo [2] - Pull all pit data from phone
-Rem echo [3] - Pull most recent match data
-Rem echo [4] - Pull most recent pit data
+echo [3] - Pull most recent match data
+echo [4] - Pull most recent pit data
+echo [8] - Access phone shell
 echo [9] - Exit 
 SET /P _inputname=Choice: 
 IF "%_inputname%"=="1" GOTO :pull
 IF "%_inputname%"=="9" GOTO :done
 IF "%_inputname%"=="2" GOTO :pullpit
-Rem IF "%_inputname%"=="3" GOTO :pullmatchrecent
-Rem IF "%_inputname%"=="4" GOTO :pullpitrecent
+IF "%_inputname%"=="3" GOTO :pullmatchrecent
+IF "%_inputname%"=="4" GOTO :pullpitrecent
+IF "%_inputname%"=="8" GOTO :shell
 
 :confused
 echo Please enter a valid choice.
@@ -32,7 +34,7 @@ mkdir matchdata
 adb shell ls sdcard/TrcScoutingApp/*.csv | tr '\r' ' ' | xargs -n1 ./adb pull -a
 move *.csv matchdata
 echo Done
-goto :start
+goto :prompt
 
 :pullpit
 echo Pulling all pit data from phone...
@@ -40,22 +42,27 @@ mkdir pitdata
 adb shell ls sdcard/TrcPitScouting/*.csv | tr '\r' ' ' | xargs -n1 ./adb pull -a
 move *.csv pitdata
 echo Done
-goto :start
+goto :prompt
 
-Rem :pullmatchrecent
-Rem echo Pulling recent match data from phone...
-Rem mkdir matchdata
-Rem adb shell ls -t sdcard/TrcScoutingApp/*.csv | tr '\r' ' ' | grep -m1 ""| xargs -n1 ./adb pull -a
-Rem move *.csv matchdata
-Rem echo Done
-Rem goto :start
+:pullmatchrecent
+echo Pulling recent match data from phone...
+mkdir matchdata
+adb shell ls sdcard/TrcScoutingApp/*.csv | tr '\r' ' ' | xargs -n1 ./adb pull -a
+ls *.csv -t | tail -n +2 | xargs rm
+move *.csv matchdata
+echo Done
+goto :prompt
 
-Rem :pullpitrecent
-Rem echo Pulling recent pit data from phone...
-Rem mkdir pitdata
-Rem adb shell ls -t sdcard/TrcScoutingApp/*.csv | tr '\r' ' ' | grep -m1 "" | xargs -n1 ./adb pull -a
-Rem move *.csv pitdata
-Rem echo Done
-Rem goto :start
+:pullpitrecent
+echo Pulling recent pit data from phone...
+mkdir pitdata
+adb shell ls sdcard/TrcPitScouting/*.csv | tr '\r' ' ' | xargs -n1 ./adb pull -a
+ls *.csv -t | tail -n +2 | xargs rm
+move *.csv pitdata
+echo Done
+goto :prompt
 
+:shell
+adb shell
+goto :prompt
 
